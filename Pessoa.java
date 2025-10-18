a9078c56efbd6d990b23bf2460243bcfeb09cf66
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-10-18 13:35:32 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,37 @@
+package modulo2;
+
+public class Pessoa {
+    // Atributos privados
+    private String nome;
+    private int idade;
+
+    // Construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Nome: " + nome + "\nIdade: " + idade;
+    }
+}
